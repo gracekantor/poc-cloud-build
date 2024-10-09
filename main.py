d3ee0c20d3171b329ed23fee165867c56a9e68d8
gracekantor <162733280+gracekantor@users.noreply.github.com>
2024-10-09 13:09:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import mysql.connector
 
 def return_basic(request):
   now = datetime.now()
-  return '<h1>Welcome to Cloud Functions and Cloud Build demo</h1><br/>' + str(now)
+  return '<h1>test function</h1><br/>' + str(now)
