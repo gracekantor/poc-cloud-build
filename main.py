2f865604533e465d9df2e3bc530bbf712fb36319
gracekantor <162733280+gracekantor@users.noreply.github.com>
2024-10-15 14:32:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import os
-from datetime import datetime
-import mysql.connector
+import functions_framework
 
-def return_basic(request):
-  now = datetime.now()
-  return '<h1>test function</h1><br/>' + str(now)
+@functions_framework.http
+def hello_get(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    Note:
+        For more information on how Flask integrates with Cloud
+        Functions, see the `Writing HTTP functions` page.
+        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
+    """
+    return 'Hello World!'
